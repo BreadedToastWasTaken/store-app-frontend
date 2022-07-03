bfed965c801a2e34584ae9dd03dd33834e6b6ad3
BreadedToast <sonicmasterman@gmail.com>
2022-07-03 17:20:56 -0400

    work on routing and making products dynamic

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Popular from "./routes/popular";
+import Categories from "./routes/categories";
+import Item from "./routes/item";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="popular" element={<Popular />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="item/:itemid" element={<Item />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>O_O Whatre You Doing Looking Here</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
